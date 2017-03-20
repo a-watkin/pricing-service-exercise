ac7d550e4a46d8bc114d2c0867ce215dcf00c5fe
Adam <atomicpenguiens@gmail.com>
2017-03-20 22:56:10 +0000

    Changed the enviromental variable key nemes in alerts.constants to work with heroku.

--- src/models/alerts/constants.py
+++ src/models/alerts/constants.py
@@ -1,8 +1,8 @@
 import os
 
-URL = os.environ.get('MAILGUN_URL')
+MAILGUN_URL = os.environ.get('MAILGUN_URL')
 # need to hide this
-API_KEY = os.environ.get('MAILGUN_API_KEY')
+MAILGUN_API_KEY = os.environ.get('MAILGUN_API_KEY')
 
 
 ALERT_TIMEOUT = 10 # minutes
@@ -12,7 +12,7 @@ COLLECTION = "alerts"
 SMPT_URL = 'smtp.mailgun.org'
 SMPT_PORT = 587
 # need to hide this also
-SMPT_KEY = os.environ.get('MAILGUN_SMPT_KEY')
+MAILGUN_SMPT_KEY = os.environ.get('MAILGUN_SMPT_KEY')
 FROM = "postmaster@sandbox91bd6a354579468a831004f9df273bcf.mailgun.org"
 
 TEST_EMAIL = 'atomicpenguines@gmail.com'
