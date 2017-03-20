90653e4c8e020fc548cf3625863cfc2494c3345a
Adam <atomicpenguiens@gmail.com>
2017-03-20 22:14:50 +0000

    Changed the alerts constants file to use environment variables.

--- src/models/alerts/constants.py
+++ src/models/alerts/constants.py
@@ -1,6 +1,8 @@
-URL = "https://api.mailgun.net/v3/sandbox91bd6a354579468a831004f9df273bcf.mailgun.org"
+import os
+
+URL = os.environ.get('MAILGUN_URL')
 # need to hide this
-API_KEY = "key-<>"
+API_KEY = os.environ.get('MAILGUN_API_KEY')
 
 
 ALERT_TIMEOUT = 10 # minutes
@@ -10,7 +12,7 @@ COLLECTION = "alerts"
 SMPT_URL = 'smtp.mailgun.org'
 SMPT_PORT = 587
 # need to hide this also
-SMPT_KEY = '<>'
+SMPT_KEY = os.environ.get('MAILGUN_SMPT_KEY')
 FROM = "postmaster@sandbox91bd6a354579468a831004f9df273bcf.mailgun.org"
 
 TEST_EMAIL = 'atomicpenguines@gmail.com'
