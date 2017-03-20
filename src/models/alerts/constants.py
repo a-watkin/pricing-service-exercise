6afa0b39b0e3d367cf3a459ed79dd77a277bd346
Adam <atomicpenguiens@gmail.com>
2017-03-20 21:34:05 +0000

    Hid the API key and SMPT key for the email service.

--- src/models/alerts/constants.py
+++ src/models/alerts/constants.py
@@ -1,5 +1,6 @@
 URL = "https://api.mailgun.net/v3/sandbox91bd6a354579468a831004f9df273bcf.mailgun.org"
-API_KEY = "key-b789f22493f73a72597faebe54fd36cd"
+# need to hide this
+API_KEY = "key-<>"
 
 
 ALERT_TIMEOUT = 10 # minutes
@@ -8,7 +9,8 @@ COLLECTION = "alerts"
 # SMPT
 SMPT_URL = 'smtp.mailgun.org'
 SMPT_PORT = 587
-SMPT_KEY = 'ca9a0bd4a3bf3a5cf8d31b0b1f8935ce'
+# need to hide this also
+SMPT_KEY = '<>'
 FROM = "postmaster@sandbox91bd6a354579468a831004f9df273bcf.mailgun.org"
 
 TEST_EMAIL = 'atomicpenguines@gmail.com'
