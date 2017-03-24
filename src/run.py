5d618f9b0961d24874d5ee37f30c5b63ae0efaf9
Adam <atomicpenguiens@gmail.com>
2017-03-24 08:13:26 +0000

    forgot to remove the setenviroment stuff that no longer works in python3 oh joy

--- src/run.py
+++ src/run.py
@@ -1,18 +1,8 @@
 from src.app import app
 import sys
-import importlib
-
-print(sys.getdefaultencoding())
-
-reload(sys)
-sys.setdefaultencoding('UTF8')
 
 
 print('encoding is', sys.getdefaultencoding())
-print('so now it will not even print anything')
 
-
-
-# importlib.reload(sys)
 
 app.run(debug=app.config["DEBUG"], port=4990)
