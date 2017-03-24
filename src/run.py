cce7384a2515a44fa86c276868addec76815120e
Adam <atomicpenguiens@gmail.com>
2017-03-24 07:48:57 +0000

    trying an older version of python 3 to see if it will use utf-8

--- src/run.py
+++ src/run.py
@@ -1,4 +1,19 @@
 from src.app import app
+import sys
+import importlib
 
+
+def set_defaultencoding_globally(encoding='utf-8'):
+    assert sys.getdefaultencoding() in ('ascii', 'mbcs', encoding)
+    import imp
+    _sys_org = imp.load_dynamic('_sys_org', 'sys')
+    _sys_org.setdefaultencoding(encoding)
+
+
+
+
+set_defaultencoding_globally()
+
+importlib.reload(sys)
 
 app.run(debug=app.config["DEBUG"], port=4990)
